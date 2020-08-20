37fd957fd6b2630f904679215f018641607adf64
yourpalenes <enesakilliok17@gmail.com>
2020-08-20 23:00:26 +0300

    Add comments

--- mysql-example-01/startup.lua
+++ mysql-example-01/startup.lua
@@ -1,4 +1,13 @@
+--[[
+    *require("mysql")
+    Description: In order to use the MySQL module, we require it into the lua file.
+]]--
 local mysql = require("mysql")
+
+--[[
+    *local settings*
+    Description: Here, a table has been opened for you to configure your MySQL settings.
+]]--
 local settings = {
     host = "",
     user = "",
@@ -7,14 +16,27 @@ local settings = {
     port = 3306
 }
 
+--[[
+    *alt.on("resourceStart")
+    Description: The following codes run when the resource is fully started.
+]]--
+
 alt.on("resourceStart",
     function()
+        --[[
+            *mysql:connect
+            Descrption: We connect to MySQL db with this function.
+        ]]--
         dbConnection = mysql:connect(settings.host, settings.user, settings.password, settings.database, settings.port)
     end
 )
 
 alt.on("resourceStop",
     function()
+         --[[
+            *dbConnect:close
+            Descrption: We disconnect because we're done.
+        ]]--
         dbConnection:close()
     end
 )
